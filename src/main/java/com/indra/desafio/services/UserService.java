e8abd4b85b7c414c47c18255aa5bc220e15d3dba
JoaoGabriielDev <jgabriellima1067@gmail.com>
2024-03-06 12:20:14 -0300

    User Service (FindAll , FindById)

---
+++ src/main/java/com/indra/desafio/services/UserService.java
@@ -0,0 +1,26 @@
+package com.indra.desafio.services;
+
+import com.indra.desafio.entities.User;
+import com.indra.desafio.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll(){
+        return repository.findAll();
+    }
+
+    public User findById(Long id){
+        Optional<User> obj = repository.findById(id);
+        return obj.get();
+    }
+
+}
