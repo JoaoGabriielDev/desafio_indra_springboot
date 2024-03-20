2ecf8a2a62976fbf8d212381293be21a4139db5c
JoaoGabriielDev <jgabriellima1067@gmail.com>
2024-03-19 22:21:09 -0300

    WebConfig(CORS)

--- src/main/java/com/indra/desafio/resources/PrecoCombustivelResource.java
+++ src/main/java/com/indra/desafio/resources/PrecoCombustivelResource.java
@@ -1,7 +1,6 @@
 package com.indra.desafio.resources;
 
 import com.indra.desafio.entities.PrecoCombustivel;
-import com.indra.desafio.entities.User;
 import com.indra.desafio.services.PrecoCombustivelService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -30,6 +29,7 @@ public class PrecoCombustivelResource {
         List<PrecoCombustivel> list = service.findAll();
         return ResponseEntity.ok().body(list);
     }
+
     @CrossOrigin(origins = {"http://localhost:8080", "http://localhost:4200"})
     @GetMapping(value = "/{id}")
     public ResponseEntity<PrecoCombustivel> findById(@PathVariable Long id){
@@ -45,12 +45,14 @@ public class PrecoCombustivelResource {
                 .path("/{id}").buildAndExpand(obj.getId()).toUri();
         return ResponseEntity.created(uri).body(obj);
     }
+
     @CrossOrigin(origins = {"http://localhost:8080", "http://localhost:4200"})
     @DeleteMapping(value = "{id}")
     public ResponseEntity<PrecoCombustivel> delete(@PathVariable Long id){
         service.delete(id);
         return ResponseEntity.noContent().build();
     }
+
     @CrossOrigin(origins = {"http://localhost:8080", "http://localhost:4200"})
     @PutMapping(value = "/{id}")
     public ResponseEntity<PrecoCombustivel> update(@PathVariable Long id, @RequestBody PrecoCombustivel obj){
@@ -58,40 +60,47 @@ public class PrecoCombustivelResource {
         return ResponseEntity.ok().body(obj);
     }
 
-    @CrossOrigin(origins = {"http://localhost:8080", "http://localhost:4200"})
+    @CrossOrigin(origins = "http://localhost:4200")
     @GetMapping("/media-por-municipio")
     public ResponseEntity<Double> obterMediaPrecoPorMunicipio(@RequestParam String municipio) {
         double mediaPreco = service.calcularMediaPrecoPorMunicipio(municipio);
         return ResponseEntity.ok(mediaPreco);
     }
+
+
     @CrossOrigin(origins = {"http://localhost:8080", "http://localhost:4200"})
     @GetMapping(value = "/por-regiao")
     public ResponseEntity<List<PrecoCombustivel>> obterPrecoPorRegiao(@RequestParam String regiao){
         List<PrecoCombustivel> precos = service.obterPrecosPorRegiao(regiao);
         return ResponseEntity.ok(precos);
     }
+
     @CrossOrigin(origins = {"http://localhost:8080", "http://localhost:4200"})
     @GetMapping("/agrupado-por-distribuidor")
     public ResponseEntity<Map<String, List<PrecoCombustivel>>> getAgrupadoPorDistribuidor() {
         Map<String, List<PrecoCombustivel>> historicoAgrupado = service.getAgrupadoPorDistribuidor();
         return ResponseEntity.ok(historicoAgrupado);
     }
+
     @CrossOrigin(origins = {"http://localhost:8080", "http://localhost:4200"})
     @GetMapping("/agrupado-por-data-coleta")
     public ResponseEntity<Map<Instant, List<PrecoCombustivel>>> obterPrecosAgrupadosPorDataColeta() {
         Map<Instant, List<PrecoCombustivel>> precosAgrupados = service.obterPrecosAgrupadosPorDataColeta();
         return ResponseEntity.ok(precosAgrupados);
     }
+
     @CrossOrigin(origins = {"http://localhost:8080", "http://localhost:4200"})
     @GetMapping("/media-compra-venda-por-municipio")
     public ResponseEntity<Map<String, Double>> obterMediaCompraVendaPorMunicipio(@RequestParam String municipio) {
         Map<String, Double> mediaPrecos = service.obterMediaCompraVendaPorMunicipio(municipio);
         return ResponseEntity.ok(mediaPrecos);
     }
+
     @CrossOrigin(origins = {"http://localhost:8080", "http://localhost:4200"})
     @GetMapping("/media-por-bandeira")
     public ResponseEntity<Map<String, Double>> obterMediaPrecosPorBandeira(@RequestParam String bandeira) {
         Map<String, Double> mediaPrecos = service.obterMediaPrecosPorBandeira(bandeira);
         return ResponseEntity.ok(mediaPrecos);
     }
+
 }
